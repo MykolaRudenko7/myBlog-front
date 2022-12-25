0b093ac5b242c1a088490c96e65be539bcaa2766
Mykola Rudenko <m.rudenko.rb@gmail.com>
2022-12-25 20:17:14 +0200

    -react-router -redux

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,39 +1,45 @@
-import React from 'react';
 import Button from '@mui/material/Button';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
-
+import styles from './Header.module.scss';
+//
+//
+//
+//
+//
+//
 export const Header = () => {
   const isAuth = false;
 
-  const onClickLogout = () => {};
+  const onClickLogout = () => { };
 
   return (
     <div className={styles.root}>
       <Container maxWidth="lg">
         <div className={styles.inner}>
-          <a className={styles.logo} href="/">
-            <div>ARCHAKOV BLOG</div>
-          </a>
+          <Link className={styles.logo} to="/">
+            <div>BLOG &#128521;</div>
+          </Link>
           <div className={styles.buttons}>
             {isAuth ? (
               <>
-                <a href="/posts/create">
-                  <Button variant="contained">Написать статью</Button>
-                </a>
+                <Link to="/posts/create">
+                  <Button variant="contained">Написати статтю</Button>
+                </Link>
                 <Button onClick={onClickLogout} variant="contained" color="error">
                   Выйти
                 </Button>
               </>
             ) : (
               <>
-                <a href="/login">
-                  <Button variant="outlined">Войти</Button>
-                </a>
-                <a href="/register">
-                  <Button variant="contained">Создать аккаунт</Button>
-                </a>
+                <Link to="/login">
+                  <Button variant="outlined">Ввійти</Button>
+                </Link>
+                <Link to="/register">
+                  <Button variant="contained">Створити аккаунт</Button>
+                </Link>
               </>
             )}
           </div>
--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import clsx from 'clsx';
 import IconButton from '@mui/material/IconButton';
+import clsx from 'clsx';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import CommentIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
 import DeleteIcon from '@mui/icons-material/Clear';
 import EditIcon from '@mui/icons-material/Edit';
 import EyeIcon from '@mui/icons-material/RemoveRedEyeOutlined';
-import CommentIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
 
-import styles from './Post.module.scss';
 import { UserInfo } from '../UserInfo';
+import styles from './Post.module.scss';
 import { PostSkeleton } from './Skeleton';
-
+//
+//
+//
+//
+//
+//
 export const Post = ({
   _id,
   title,
@@ -28,17 +35,17 @@ export const Post = ({
     return <PostSkeleton />;
   }
 
-  const onClickRemove = () => {};
+  const onClickRemove = () => { };
 
   return (
     <div className={clsx(styles.root, { [styles.rootFull]: isFullPost })}>
       {isEditable && (
         <div className={styles.editButtons}>
-          <a href={`/posts/${_id}/edit`}>
+          <Link to={`/posts/${_id}/edit`}>
             <IconButton color="primary">
               <EditIcon />
             </IconButton>
-          </a>
+          </Link>
           <IconButton onClick={onClickRemove} color="secondary">
             <DeleteIcon />
           </IconButton>
@@ -55,12 +62,12 @@ export const Post = ({
         <UserInfo {...user} additionalText={createdAt} />
         <div className={styles.indention}>
           <h2 className={clsx(styles.title, { [styles.titleFull]: isFullPost })}>
-            {isFullPost ? title : <a href={`/posts/${_id}`}>{title}</a>}
+            {isFullPost ? title : <Link to={`/posts/${_id}`}>{title}</Link>}
           </h2>
           <ul className={styles.tags}>
             {tags.map((name) => (
               <li key={name}>
-                <a href={`/tag/${name}`}>#{name}</a>
+                <Link to={`/tag/${name}`}>#{name}</Link>
               </li>
             ))}
           </ul>
